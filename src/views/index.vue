addfd67833f8072527d3457ae3d7239bcd722b12
Bmaili <2228921046@qq.com>
2023-02-10 20:44:29 +0800

    大屏看板

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,8 +1,28 @@
 <template>
-  <h2>首页</h2>
+  <div class="network_container">
+    <iframe class="network_show" name="content_frame" src="https://console.huaweicloud.com/dlv/vision/share/?id=976e712fd4104276a1de2c1121d93cd8&locale=zh-cn&region=cn-south-1" frameborder=0  scrolling="no"></iframe>
+  </div>
 </template>
+
 <script>
 export default {
 
 }
 </script>
+<style scoped>
+div{	/* div的CSS样式 */
+  position : absolute;
+  width : 100%;
+  height : 100%;
+}
+iframe{	/* div的CSS样式 */
+  position : absolute;
+  width : 100%;
+  height : 100%;
+}
+*{		/* CSS Reset */
+  margin : 0;
+  padding : 0;
+}
+
+</style>
